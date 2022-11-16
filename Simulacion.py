1a89fcb9ac2988c62e012bcec76e5085cbe421f8
jose manuel <jose.ibaceta@usach.cl>
2022-11-16 18:49:30 -0300

    limpieza de prints de prueba

--- Simulacion.py
+++ Simulacion.py
@@ -1,12 +1,8 @@
 from fondas_simulacion import FondaModel
 f = open ('datos.txt','r')
 datos = f.read()
-print(datos.split('\n'))
 NC,NV,cantMaxVendedores,tiempoAtencion,Comida=datos.split('\n')
 Comida=Comida.split(',')
 f.close()
 simulacion=FondaModel(NC,NV,cantMaxVendedores,tiempoAtencion,Comida)
-print(simulacion.horario)
-for i in range(len(simulacion.ambiente)):
-    print(simulacion.ambiente[0])
 simulacion.step()
