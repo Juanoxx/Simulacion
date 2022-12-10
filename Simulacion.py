31c89d4e745fa44d4a8a49313f46e31ef6b4382d
jose manuel <jose.ibaceta@usach.cl>
2022-12-09 23:33:54 -0300

    listo pero falta arreglar graficos

--- Simulacion.py
+++ Simulacion.py
@@ -1,12 +1,30 @@
 from fondas_simulacion import FondaModel
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import cv2
+from os import remove
+from os import path
 f = open ('datos.txt','r')
 datos = f.read()
 NC,NV,cantMaxVendedores,tiempoAtencion,Comida=datos.split('\n')
 Comida=Comida.split(',')
 f.close()
 simulacion=FondaModel(NC,NV,cantMaxVendedores,tiempoAtencion,Comida)
-simulacion.step()
-for i in range(1000):
+print(simulacion.schedule.agents[40].unique_id)
+for i in range(13*60):
    simulacion.step()
-#for i in range(simulacion.schedule.get_agent_count()):
-   # print(simulacion.schedule.agents[i].unique_id)
+print(simulacion.visitas)
+print(simulacion.CantidadProductosVendidos)
+imagenes=[]
+for j in range(13*60):
+   imagenes.append(cv2.imread(str(j+1)+'.png'))
+height, width  = imagenes[9].shape[:2]
+video = cv2.VideoWriter('recorrido.wmv',cv2.VideoWriter_fourcc(*'mp4v'),2,(width,height))
+for k in range(13*60):
+   video.write(imagenes[k])
+   if path.exists(str(k+1)+".png"):
+    remove(str(k+1)+".png")
+video.release()
--- fondas_simulacion.py
+++ fondas_simulacion.py
@@ -1,6 +1,10 @@
 import mesa
 import random
 import time
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
 # Agentes: Consumidores y Locales de venta
 def crearMatriz(x,y):
     matriz=[]
@@ -23,42 +27,91 @@ class ConsumidoresAgent(mesa.Agent):
         self.posicion=[0,0]
         self.cantidadPersonas=0
         self.espera=0
+        self.enFonda=False
         #agregar numero de personas por agente
     def setDatosIniciales(self):
         for i in range(len(self.model.comida)):
-            self.deseoCompra.append(round(random.uniform(-1,1),2))
+            self.deseoCompra.append([round(random.uniform(-1,1),2),i])
             self.tiempoEntrada=random.randint(1,self.model.horario)
             self.tiempoSalida=random.randint(self.tiempoEntrada,self.model.horario)
             self.Inicial=self.model.entradas[random.randint(0,2)]
             self.cantidadPersonas=random.randint(1,5)
             #self.posicion=self.Inicial
-        print(self.tiempoEntrada,"este es mi tiempo de entrada")
+        #print(self.tiempoEntrada,"este es mi tiempo de entrada")
+    
+        
+    def step(self):
+        #print('funciono y soy el consumidor ', str(self.unique_id),'y mi entrada es:\n',self.Inicial)
+        if(self.model.stepCounter==self.tiempoEntrada):
+            self.entrar()
+            #print('Soy el consumidor ', str(self.unique_id),'y mi entrada es:\n',self.Inicial)
+        elif(self.model.stepCounter>self.tiempoEntrada and self.model.stepCounter<self.tiempoSalida):
+            #print('Soy el consumidor ', str(self.unique_id),'y me muevo')
+            if(self.model.ambiente[self.posicion[0]][self.posicion[1]]!=0  ):
+                #print("debug 1",self.unique_id)
+                if(self.espera!=0):
+                    #print("debug espera")
+                    if(self.espera==1):
+                        #print("debug 2",self.unique_id)
+                        puesto=self.model.ambiente[self.posicion[0]][self.posicion[1]]
+                        self.model.schedule.agents[puesto].fila-=1
+                        self.comprar()
+                        self.moverse()
+                        
+                    else:
+                        #print("debug 3",self.unique_id)
+                        self.espera-=1
+                else:
+                    #print("debug else",self.unique_id)
+                    puesto=self.model.ambiente[self.posicion[0]][self.posicion[1]]
+                    #print(puesto)
+                    if(self.model.schedule.agents[puesto].fila < 10):                        
+                        self.espera=self.model.tiempoAtencion
+                        self.model.visitas[puesto-self.model.numConsumidores]+=1
+                        self.model.schedule.agents[puesto].fila+=1
+                    else:
+                        self.moverse()
+            else:
+                self.moverse()            
+        elif ( (self.model.stepCounter==self.model.horario or self.model.stepCounter==self.tiempoSalida) and self.enFonda):
+            self.salir()
+    def comprar(self):
+        self.deseoCompra.sort(reverse=True)
+        puesto=self.model.ambiente[self.posicion[0]][self.posicion[1]]
+        counter=0
+        while(True):
+            if(self.model.schedule.agents[puesto].CantidadProductosDisponibles[self.deseoCompra[counter][1]]>self.cantidadPersonas):
+                venta=round(random.uniform(0,1),2)
+                print("venta:", venta, "\noportunidad:",self.model.schedule.agents[puesto].oportunidadVenta[self.deseoCompra[counter][1]])
+                if(venta<=self.model.schedule.agents[puesto].oportunidadVenta[self.deseoCompra[counter][1]]):
+                    self.model.schedule.agents[puesto].CantidadProductosDisponibles[self.deseoCompra[counter][1]]-=self.cantidadPersonas
+                    self.model.CantidadProductosVendidos[self.deseoCompra[counter][1]]+=self.cantidadPersonas
+                    self.deseoCompra[counter][0]=-1.1
+                    print("lo compro")
+                break
+            counter+=1
+            if(counter==len(self.model.comida)):
+                print("no compro")
+                break
     def moverse(self):
         mov=self.movimientos[random.randint(0,7)]
         nuevaPos=[(self.posicion[0]+mov[0]),(self.posicion[1]+mov[1])]
         if (nuevaPos[0]>8 or nuevaPos[0]<0 or nuevaPos[1]>48 or nuevaPos[1]<0 or nuevaPos==self.ultimaPos):
             return self.moverse()
-        print(self.posicion,'vieja pos')
-        print(mov,'movimiento')
-        print(nuevaPos,'nueva pos')
+        #print(self.posicion,'vieja pos')
+        #print(mov,'movimiento')
+        #print(nuevaPos,'nueva pos')
         self.model.ambienteCantidadPersonas[nuevaPos[0]][nuevaPos[1]] += self.cantidadPersonas
         self.model.ambienteCantidadPersonas[self.posicion[0]][self.posicion[1]] -= self.cantidadPersonas
         self.ultimaPos=self.posicion
         self.posicion=nuevaPos
-        
-    def step(self):
-        #print('funciono y soy el consumidor ', str(self.unique_id),'y mi entrada es:\n',self.Inicial)
-        if(self.model.stepCounter==self.tiempoEntrada):
-            self.posicion=self.Inicial
-            self.model.ambienteCantidadPersonas[self.posicion[0]][self.posicion[1]] += self.cantidadPersonas
-            print('Soy el consumidor ', str(self.unique_id),'y mi entrada es:\n',self.Inicial)
-        if(self.model.stepCounter>self.tiempoEntrada and self.model.stepCounter<self.tiempoSalida):
-            print('Soy el consumidor ', str(self.unique_id),'y me muevo')
-            self.moverse()
-        if (self.model.stepCounter>=self.model.horario or self.model.stepCounter==self.tiempoSalida):
-            self.salir()
+    def entrar(self):
+        self.posicion=self.Inicial
+        self.model.ambienteCantidadPersonas[self.posicion[0]][self.posicion[1]] += self.cantidadPersonas
+        self.enFonda=True
     def salir(self):
-        print('Tengo que salir')
+        self.enFonda=False
+        self.model.ambienteCantidadPersonas[self.posicion[0]][self.posicion[1]] -= self.cantidadPersonas
 class LocalVentaAgent(mesa.Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
@@ -68,7 +121,6 @@ class LocalVentaAgent(mesa.Agent):
         self.posicion=[]
         #Tamaño maximo de fila es 10.
         self.fila=0
-        self.visitas=0
     def setDatosIniciales(self):
         self.CantidadVendedores=random.randint(1,self.model.cantMaxVendedores)
         for i in range(len(self.model.comida)):
@@ -93,13 +145,15 @@ class FondaModel(mesa.Model):
         self.numConsumidores = int(NC)
         self.numLocalesVenta = int(NV)
         self.cantMaxVendedores=int(cantMaxVendedores)
-        self.horario=1000#13*60
+        self.horario=13*60
         self.comida=Comida
         self.tiempoAtencion=int(tiempoAtencion)
         self.stepCounter=0
         self.ambiente=crearMatriz(9,49)
         self.ambienteCantidadPersonas=crearMatriz(9,49)
-        self.entradas=[[4,0],[8,27],[6,48]]
+        self.entradas=[[4,0],[8,27],[6,48]]        
+        self.CantidadProductosVendidos=[]
+        self.visitas=[]
         self.schedule = mesa.time.RandomActivation(self)
         for i in range(self.numConsumidores):
             a= ConsumidoresAgent(i, self)
@@ -109,13 +163,27 @@ class FondaModel(mesa.Model):
             b=LocalVentaAgent(self.numConsumidores+j,self)
             b.setDatosIniciales()
             self.schedule.add(b)
+            self.visitas.append(0)
+            #print(b.unique_id)      
+        
+        for k in range(len(self.comida)):
+            self.CantidadProductosVendidos.append(0)
+        mapa = pd.DataFrame(
+            self.ambiente
+        )
+        print(mapa)
         input("Press Enter to continue...")
     def step(self):
         self.stepCounter+=1
         self.schedule.step()
-        for i in range(len(self.ambienteCantidadPersonas)):
-            print('\n')
-            print(self.ambienteCantidadPersonas[i])
         print(self.stepCounter)
-        input("Press Enter to continue...")
-        
+        usuarios = pd.DataFrame(
+            self.ambienteCantidadPersonas
+        )
+
+        plt.figure(self.stepCounter, figsize=(27,5))
+        ax = sns.heatmap(usuarios, cmap = "Blues", annot = True)
+        plt.savefig(str(self.stepCounter)+'.png')
+        fig=plt.figure()
+        matplotlib.use("Agg")
+        #input("Press Enter to continue..."
